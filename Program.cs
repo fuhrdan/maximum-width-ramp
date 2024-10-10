e073ddb1aee8646ef9def3494b459923440a9d04
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-10-10 10:08:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//*****************************************************************************
+//** 962. Maximum Width Ramp    leetcode                                     **
+//*****************************************************************************
+
+
+int maxWidthRamp(int* nums, int numsSize) {
+    int* stack = (int*)malloc(numsSize * sizeof(int));
+    int stackSize = 0;
+    int widestRamp = 0;
+    
+    // Build a decreasing stack of indices
+    for (int i = 0; i < numsSize; i++) {
+        if (stackSize == 0 || nums[stack[stackSize - 1]] > nums[i]) {
+            stack[stackSize++] = i;
+        }
+    }
+    
+    // Traverse the array from right to left
+    for (int j = numsSize - 1; j >= 0; j--) {
+        // Check if we can form a ramp with the current element nums[j]
+        while (stackSize > 0 && nums[stack[stackSize - 1]] <= nums[j]) {
+            int i = stack[--stackSize]; // pop the top element
+            widestRamp = (widestRamp > (j - i)) ? widestRamp : (j - i);
+        }
+    }
+    
+    free(stack);
+    return widestRamp;
+}
